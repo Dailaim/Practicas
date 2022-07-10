c1c17439e28c7ee84156aa3931468be23644317f
Daizai <leo-im@hotmail.com>
2022-07-09 21:48:00 -0500

    commit

---
+++ nETX/Js/7.1.js
@@ -0,0 +1,24 @@
+var palabra;
+var expresionRegular;
+
+expresionRegular = /as/i
+
+palabra = "pASa";
+
+console.log(expresionRegular.test(palabra))
+
+palabra = "al1"
+
+expresionRegular = new RegExp('^[abc]l[0-9]')
+
+var resultado = palabra.match(expresionRegular)
+console.log(resultado)
+
+
+
+
+
+
+
+
+9
